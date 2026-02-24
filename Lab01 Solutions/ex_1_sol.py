908c1732780123b8f7875a524ba0288beb3e3303
Silvana Zhao <silvanazhao@Silvanas-MacBook-Pro.local>
2026-02-24 22:15:25 +1100

    Update Lab01 solutions

--- Lab01 Solutions/ex_1_sol.py
+++ Lab01 Solutions/ex_1_sol.py
@@ -1,9 +1,14 @@
-def f1_1(m: int, n: int) -> str:
+def f1(m: int, n: int) -> str:
     """
     Creates a pattern of m segments,
     each containing a pair of vertical bars with n underscores between them.
+    :param m: (int) Number of segments to repeat.
+    :param n: (int) Number of underscores inside each segment.
+    :return:
+        str: The resulting pattern string formed by repeating the segment `m` times.
     """
-    return ("|" + "_" * n + "|") * m
+    segment = "|" + "_" * n + "|" # Build one segment: | + n underscores + |
+    return segment * m # Repeat the segment m times to form the full pattern
 
 
 def f1_2(m: int, n: int) -> str:
--- Lab01 Solutions/ex_3_sol.py
+++ Lab01 Solutions/ex_3_sol.py
@@ -27,3 +27,31 @@ def f3_2(L: list[int]) -> None:
     # filter them out from the list
     result = [x for x in L if x not in to_delete]
     print(result)
+
+
+def f3_3(L: list[int]) -> None:
+    """
+    Removes elements from L using a while loop and remove method.
+    Checks from right to left, removing elements smaller than their successors.
+    """
+    if len(L) <= 1:
+        print(L)
+        return
+
+    e = L[-1]  # get the last element e
+
+    i = len(L) - 2  # index for second last element in L and move leftwards later in the while loop
+    to_remove = []  # collect all x
+
+    # scan from the end as long as L[i] < e
+    while len(L) > 1 and L[i] < e:
+        to_remove.append(L[i])
+        i = i - 1
+
+    result = []  # build new list to keep numbers not in to_remove
+
+    for num in L:
+        if num not in to_remove:
+            result.append(num)
+
+    print(result)
